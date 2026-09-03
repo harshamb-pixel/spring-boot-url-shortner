3e31fe8f7761e2c14052e006254136e4f9a570a7
Harsha M B <harshablgowda@gmail.com>
2026-09-04 01:35:18 +0530

    implement registration with JdbcClient

---
+++ src/main/java/com/harsha/spring_boot_url_shortner/web/dtos/RegisterUserRequest.java
@@ -0,0 +1,16 @@
+package com.harsha.spring_boot_url_shortner.web.dtos;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record RegisterUserRequest(
+        @NotBlank(message = "Email is required")
+        @Email(message = "Invalid email format")
+        String email,
+        @NotBlank(message = "Password is required")
+        String password,
+        @NotBlank(message = "Name is required")
+        String name
+) {
+}
