a282a016c1af2fc720070870a46bacfb4c779962
Harsha M B <harshablgowda@gmail.com>
2026-09-04 00:11:55 +0530

    add pagination to public short URLs

--- src/main/java/com/harsha/spring_boot_url_shortner/domain/repositories/ShortUrlRepository.java
+++ src/main/java/com/harsha/spring_boot_url_shortner/domain/repositories/ShortUrlRepository.java
@@ -3,12 +3,17 @@ import com.harsha.spring_boot_url_shortner.domain.entities.ShortUrl;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
-    @Query("select su from ShortUrl su left join fetch su.createdBy where su.isPrivate = false order by su.createdAt desc")
-    List<ShortUrl> findPublicShortUrls();
+    @Query("select su from ShortUrl su left join fetch su.createdBy where su.isPrivate = false")
+    Page<ShortUrl> findPublicShortUrls(Pageable pageable);
 
     boolean existsByShortKey(String shortKey);
 
--- src/main/java/com/harsha/spring_boot_url_shortner/web/controllers/HomeController.java
+++ src/main/java/com/harsha/spring_boot_url_shortner/web/controllers/HomeController.java
@@ -4,6 +4,7 @@ package com.harsha.spring_boot_url_shortner.web.controllers;
 import com.harsha.spring_boot_url_shortner.domain.entities.ShortUrl;
 import com.harsha.spring_boot_url_shortner.domain.entities.User;
 import com.harsha.spring_boot_url_shortner.domain.exceptions.ShortUrlNotFoundException;
+import com.harsha.spring_boot_url_shortner.domain.models.PagedResult;
 import com.harsha.spring_boot_url_shortner.domain.services.ShortUrlService;
 import com.harsha.spring_boot_url_shortner.domain.services.ShortUrlService;
 
@@ -15,10 +16,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -30,20 +28,28 @@ public class HomeController {
     private final ApplicationProperties properties;
     private final SecurityUtils securityUtils;
 
-    public HomeController(ShortUrlService shortUrlService, ApplicationProperties properties, SecurityUtils securityUtils) {
+    public HomeController(ShortUrlService shortUrlService,
+                          ApplicationProperties properties,
+                          SecurityUtils securityUtils) {
         this.shortUrlService = shortUrlService;
         this.properties = properties;
         this.securityUtils = securityUtils;
     }
 
     @GetMapping("/")
-    public String home(Model model) {
-        List<ShortUrlDto> shortUrls = shortUrlService.findAllPublicShortUrls();
-        model.addAttribute("shortUrls", shortUrls);
-        model.addAttribute("baseUrl", properties.baseUrl());
+    public String home(
+            @RequestParam(defaultValue = "1") Integer page,
+            Model model) {
+        this.addShortUrlsDataToModel(model, page);
         model.addAttribute("createShortUrlForm",
                 new CreateShortUrlForm("", false, null));
         return "index";
+    }
+
+    private void addShortUrlsDataToModel(Model model, int pageNo) {
+        PagedResult<ShortUrlDto> shortUrls = shortUrlService.findAllPublicShortUrls(pageNo, properties.pageSize());
+        model.addAttribute("shortUrls", shortUrls);
+        model.addAttribute("baseUrl", properties.baseUrl());
     }
 
     @PostMapping("/short-urls")
@@ -52,9 +58,7 @@ public class HomeController {
                           RedirectAttributes redirectAttributes,
                           Model model) {
         if(bindingResult.hasErrors()) {
-            List<ShortUrlDto> shortUrls = shortUrlService.findAllPublicShortUrls();
-            model.addAttribute("shortUrls", shortUrls);
-            model.addAttribute("baseUrl", properties.baseUrl());
+            this.addShortUrlsDataToModel(model, 1);
             return "index";
         }
 
